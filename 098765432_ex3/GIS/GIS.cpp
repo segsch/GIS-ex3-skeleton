5e1c7b376cb3a7b15d48e23eb76af54b133e5a85
root <root@nova.cs.tau.ac.il>
2021-01-02 11:27:37 +0200

    Clean demo stubs

--- 098765432_ex3/GIS/GIS.cpp
+++ 098765432_ex3/GIS/GIS.cpp
@@ -8,35 +8,26 @@ REGISTER_GIS(GIS_098765432) // Registering the GIS
 std::vector<EntityId> GIS_098765432::loadMapFile(const std::string& filename) {;
 	// This is a stub
 	std::cout << "I, GIS_098765432, was requested to open: " << filename << ". I can't do anything with it yet...\n";
-	std::vector<EntityId> empty_vector{};
-	return empty_vector;	
+	return {};	
 }
 
-std::vector<EntityId> GIS_098765432::getWaysByJunction(const EntityId& junctionId) const {
+std::vector<EntityId> GIS_098765432::getWaysByJunction(const EntityId&) const {
 	// This is a stub
-	(void)junctionId;
-	std::vector<EntityId> returned{EntityId{"W1"}, EntityId{"W2"}};
-	return returned;
+	return {EntityId{"W1"}, EntityId{"W2"}};
 }
 
-std::tuple<Coordinates, EntityId, std::size_t>GIS_098765432::getWayClosestPoint(const Coordinates& c) const {
+std::tuple<Coordinates, EntityId, std::size_t>GIS_098765432::getWayClosestPoint(const Coordinates&) const {
 	// This is a stub
-	std::tuple<Coordinates, EntityId, std::size_t> returned = std::make_tuple(Coordinates{Longitude{0},Latitude{0}}, EntityId{"other"}, (std::size_t) 5);
-	(void)c;
-	return returned;
+	return std::make_tuple(Coordinates{Longitude{0},Latitude{0}}, EntityId{"other"}, (std::size_t) 5);
 }
 
-std::tuple<Coordinates, EntityId, std::size_t>GIS_098765432::getWayClosestPoint(const Coordinates& c, const Restrictions& res) const {
+std::tuple<Coordinates, EntityId, std::size_t>GIS_098765432::getWayClosestPoint(const Coordinates& c, const Restrictions&) const {
 	// This is a stub
-	std::tuple<Coordinates, EntityId, std::size_t> returned = std::make_tuple(Coordinates{Longitude{0},Latitude{0}}, EntityId{"other"}, (std::size_t) 5);
-	(void)c;
-	(void)res;
-	return returned;
+	return std::make_tuple(c, EntityId{"other"}, (std::size_t) 5);
 }
 
-const AbstractWay& GIS_098765432::getWay(const EntityId& wayId) const {
+const AbstractWay& GIS_098765432::getWay(const EntityId&) const {
 	// This is a stub
-	(void)wayId;
 	static Way dummy_way{};
 	return dummy_way;
 }
