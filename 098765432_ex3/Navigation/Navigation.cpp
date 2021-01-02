1f3399e69ad860a22a1b7505a50b1f01697c2add
root <root@nova.cs.tau.ac.il>
2021-01-02 11:31:07 +0200

    REGISTER_NAVIGATION and add additional usage demo in getRoutes

--- 098765432_ex3/Navigation/Navigation.cpp
+++ 098765432_ex3/Navigation/Navigation.cpp
@@ -1,8 +1,11 @@
 #include "Navigation.h"
+#include "../Common/NavigationRegistration.h"
+#include "../Common/AbstractGIS.h"
+#include <tuple>
 #include <memory>
-#include <iostream>
+#include <iostream> // For demo only
 
-Navigation_098765432::Navigation_098765432(const NavigationGIS& gis) { (void)gis; }
+REGISTER_NAVIGATION(Navigation_098765432)
 
 /**
  * @brief Get the Routes object
@@ -12,11 +15,17 @@ Navigation_098765432::Navigation_098765432(const NavigationGIS& gis) { (void)gis
  * @return Routes 
  */
 std::unique_ptr<AbstractRoutes> Navigation_098765432::getRoutes(const Coordinates& start, const Coordinates& end) const {
-    // A stub
-    std::cout << "Navigation_098765432 is Getting routes from...\n";
-    (void)start;
-    (void)end;
-    return std::make_unique<Routes>();
+    // For demo only
+    std::cout << "Navigation_098765432 is Getting routes...\n";
+    auto start_point = gis.getWayClosestPoint(start);
+    gis.getWayClosestPoint(end);
+    auto start_coordinates = std::get<AbstractGIS::ClosestPoint::Coord>(start_point);
+    auto start_way_id = std::get<AbstractGIS::ClosestPoint::WayId>(start_point);
+    auto start_segment_index = std::get<AbstractGIS::ClosestPoint::SegmentIndex>(start_point);
+    const auto& start_way = gis.getWay(start_way_id);
+    [[maybe_unused]] 
+    std::pair<Meters, Meters> start_way_partition = start_way.getSegmentPartsOnWay(start_segment_index, start_coordinates);
+    return std::make_unique<Routes>(); 
 }
 
 
